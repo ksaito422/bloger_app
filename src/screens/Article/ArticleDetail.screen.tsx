9daf4e403b91f6ab138372e2b9c64af875cc04a9
saito.k <k.saito@impl.co.jp>
2021-11-02 10:52:14 +0900

    add #24

--- src/screens/Article/ArticleDetail.screen.tsx
+++ src/screens/Article/ArticleDetail.screen.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Card, Icon } from 'react-native-elements';
+import useSWR from 'swr';
 
 import { useNav } from 'src/hooks/useNav';
 import { SPACE, COLOR } from 'src/styles';
-
+import { EP, fetcherGet } from 'src/services/fetcher';
 import { Spacing } from 'src/components/common/Spacing';
 
 export const ArticleDetailScreen = ({ route }) => {
 	const { navigate } = useNav();
 	const routeParams = route.params;
+
+	const { data, error } = useSWR<Event>(EP.articleDetail(routeParams.articleId), fetcherGet);
 
 	return (
 		<SafeAreaView edges={['bottom']} style={styles.safeAreaView}>
@@ -19,7 +22,7 @@ export const ArticleDetailScreen = ({ route }) => {
 
 				<Card containerStyle={styles.card}>
 					<View style={styles.infoView}>
-						<Text style={styles.info}>2021年1月1日</Text>
+						<Text style={styles.info}>{data?.createdAt}</Text>
 						<View style={styles.iconView}>
 							<Icon name="edit" size={20} onPress={() => navigate('ArticleEdit')} />
 							<Icon name="delete-outline" size={20} onPress={() => console.log('削除')} />
@@ -27,10 +30,10 @@ export const ArticleDetailScreen = ({ route }) => {
 					</View>
 					<Spacing vertical={1} />
 
-					<Card.Title style={styles.title}>記事タイトルが入ります</Card.Title>
+					<Card.Title style={styles.title}>{data?.title}</Card.Title>
 					<Card.Divider />
 
-					<Text>記事の内容が入ります。記事の内容が入ります。記事の内容が入ります。</Text>
+					<Text>{data?.content}</Text>
 				</Card>
 			</ScrollView>
 		</SafeAreaView>
