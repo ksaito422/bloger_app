b8fb2c890f451a7024cc450528f6defa8201487a
saitooooooo <kitsit.39@gmail.com>
2021-11-03 12:07:40 +0900

    add #11

--- src/components/organisms/ArticlePost.tsx
+++ src/components/organisms/ArticlePost.tsx
@@ -6,19 +6,15 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { COLOR } from 'src/styles';
 import { ArticlePostType } from 'src/types';
 import { ArticlePostSchema } from 'src/schema';
-
+import { useArticle } from 'src/hooks/useArticle';
 import { RhfInput } from 'src/components/form/RhfInput';
 import { Button } from 'src/components/button/Button';
 
-type FormInfoType = {
-	title: string;
-	content: string;
-};
-
 export const ArticlePost: FC<ArticlePostType> = ({ title, content }) => {
 	const { control, handleSubmit } = useForm({ resolver: yupResolver(ArticlePostSchema) });
+	const { postArticle } = useArticle();
 
-	const onSubmit: SubmitHandler<FormInfoType> = formInfo => console.log(formInfo);
+	const onSubmit: SubmitHandler<ArticlePostType> = formInfo => postArticle(formInfo);
 
 	return (
 		<View style={styles.container}>
---
+++ src/hooks/useArticle.ts
@@ -0,0 +1,32 @@
+import auth from '@react-native-firebase/auth';
+import { useRecoilValue } from 'recoil';
+
+import { API } from 'src/config/apiConfig';
+import { EP } from 'src/services/fetcher';
+import { ArticlePostType } from 'src/types';
+import { userIdAtom } from 'src/store/atoms';
+
+export const useArticle = () => {
+	const userId = useRecoilValue(userIdAtom);
+
+	const postArticle = async (formInfo: ArticlePostType) => {
+		try {
+			const idToken = await auth().currentUser?.getIdToken();
+
+			await API.post(EP.postArticle(userId), formInfo, {
+				headers: { Authorization: 'Bearer' + ' ' + idToken },
+			})
+				.then(res => {
+					return res;
+				})
+				.catch(e => {
+					console.log(e);
+					throw new Error(e);
+				});
+		} catch (e) {
+			console.log(e);
+		}
+	};
+
+	return { postArticle };
+};
